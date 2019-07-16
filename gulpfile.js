17c82d58f09063d210e21e5bfb6dca09a493cf07
granit-sales <deinekomisha1990@gmail.com>
2019-07-17 00:05:24 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+//Подключаем модули галпа
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+//Порядок подключения css файлов
+const cssFiles = [
+	'styles.css'
+]
+//Порядок подключения js файлов
+const jsFiles = [
+  'main.js',
+  'full.js'
+]
+
+const HTMLFiles = [
+  'index.html'
+]
+
+//Таск на стили CSS
+function styles() {
+   //Шаблон для поиска файлов CSS
+   //Всей файлы по шаблону './src/css/**/*.css'
+   return gulp.src(cssFiles)
+   //Объединение файлов в один
+   .pipe(concat('style.css'))
+   //Добавить префиксы
+   .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+   }))
+   //Минификация CSS
+   .pipe(cleanCSS({
+      level: 2
+   }))
+   //Выходная папка для стилей
+   .pipe(gulp.dest('./src/styles'))
+   .pipe(browserSync.stream());
+}
+
+//Таск на скрипты JS
+function scripts() {
+   //Шаблон для поиска файлов JS
+   //Всей файлы по шаблону './src/js/**/*.js'
+   return gulp.src(jsFiles)
+   //Объединение файлов в один
+   .pipe(concat('script.js'))
+   //Минификация JS
+   .pipe(uglify({
+      toplevel: true
+   }))
+   //Выходная папка для скриптов
+   .pipe(gulp.dest('./src/script'))
+   .pipe(browserSync.stream());
+}
+
+//Удалить всё в указанной папке
+function clean() {
+   return del(['busket/*'])
+}
+
+//Просматривать файлы
+function watch() {
+   browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+
+  //Следить за CSS файлами
+  gulp.watch('styles.css', styles)
+  //Следить за JS файлами
+  gulp.watch('main.js', scripts)
+  //При изменении HTML запустить синхронизацию
+  gulp.watch("./*.html").on('change', browserSync.reload);
+
+
+}
+
+//Таск вызывающий функцию styles
+gulp.task('styles', styles);
+//Таск вызывающий функцию scripts
+gulp.task('scripts', scripts);
+//Таск для очистки папки build
+gulp.task('del', clean);
+//Таск для отслеживания изменений
+gulp.task('watch', watch);
+//Таск для удаления файлов в папке build и запуск styles и scripts
+gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts)));
+//Таск запускает таск build и watch последовательно
+gulp.task('dev', gulp.series('build','watch'));
